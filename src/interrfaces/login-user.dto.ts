efbecc28dc6bad0ec538f56289f39363575340ab
Andrea Noer <andreanoer@gmail.com>
2024-09-18 01:09:18 -0300

    refactor separando interfaces

---
+++ src/interrfaces/login-user.dto.ts
@@ -0,0 +1,7 @@
+export interface LoginUserInputDTO {
+  email: string;
+  password: string;
+}
+export interface LoginUserOutputDTO {
+  authToken: string;
+}
--- src/services/auth.service.ts
+++ src/services/auth.service.ts
@@ -1,15 +1,11 @@
 import { prismaConnection } from "../database/prismaConnection";
 import { HttpError } from "../errors/http.error";
+import {
+  LoginUserInputDTO,
+  LoginUserOutputDTO,
+} from "../interrfaces/login-user.dto";
 import { Bcryt } from "../libs/bcrypt.lib";
 import { JWT } from "../libs/jwt.lib";
-
-interface LoginUserInputDTO {
-  email: string;
-  password: string;
-}
-interface LoginUserOutputDTO {
-  authToken: string;
-}
 
 export class AuthService {
   public async loginUser(
