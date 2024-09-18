070cdd51bbb66de56ed8ca4ceda4ffe227758bbc
Andrea Noer <andreanoer@gmail.com>
2024-09-18 00:31:39 -0300

    refactor do login de user

---
+++ src/services/auth.service.ts
@@ -0,0 +1,45 @@
+import { prismaConnection } from "../database/prismaConnection";
+import { HttpError } from "../errors/http.error";
+import { Bcryt } from "../libs/bcrypt.lib";
+import { JWT } from "../libs/jwt.lib";
+
+interface LoginUserInputDTO {
+  email: string;
+  password: string;
+}
+interface LoginUserOutputDTO {
+  authToken: string;
+}
+
+export class AuthService {
+  public async loginUser(
+    input: LoginUserInputDTO
+  ): Promise<LoginUserOutputDTO> {
+    const userFound = await prismaConnection.users.findUnique({
+      where: {
+        email: input.email,
+        deleted: false,
+      },
+    });
+    //verifica email
+    if (!userFound) {
+      throw new HttpError("Invalid Credentials", 401);
+    }
+    //verifica senha
+    const bcrypt = new Bcryt();
+    const isMatch = await bcrypt.verify(userFound.password, input.password);
+
+    if (!isMatch) {
+      throw new HttpError("Invalid Credentials", 401);
+    }
+
+    const jwt = new JWT();
+    const token = jwt.generateToken({
+      id: userFound.id,
+      name: userFound.name,
+      email: userFound.email,
+      username: userFound.username,
+    });
+    return { authToken: token };
+  }
+}
